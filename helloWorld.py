bd8327dac77055403d67175ab7e8fedf57a030ab
MuhireIghor <muhireighor123@gmail.com>
2022-10-31 22:09:06 +0200

    initial commit

---
+++ helloWorld.py
@@ -0,0 +1,29 @@
+print("Hello World")
+print("Hello World")
+x=4
+y=2
+if y<x:
+    print("4 is greater than 2")
+if y<x:
+            print("4 is greater than 2")
+name="Muhire"
+print(type(name))
+floatNumber=2.34
+print(type(floatNumber))
+intNumber = 7
+print(type(intNumber))
+location = 'Kigali'
+gender="Male"
+print(location)
+print(gender)
+Location=2
+print(Location)
+#variable assignment methods
+district,sector,country = "Gasabo",'Kimironko',"Rwanda"
+print(district)
+print(sector)
+print(country)
+x="Python "
+y=" is "
+z=" awesome "
+print(x, y,z)
